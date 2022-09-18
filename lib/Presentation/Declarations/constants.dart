2eaa149974b7dd2ef05a55271a6306ea92a75234
vijayinyoutube <vijaythevirtue@gmail.com>
2022-09-18 11:45:40 +0530

    refactor

--- lib/Presentation/Declarations/constants.dart
+++ lib/Presentation/Declarations/constants.dart
@@ -1,10 +1,8 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
-Color primaryColor = const Color(0xff293462);
-Color secondaryColor = const Color(0xff243A73);
-Color accentColor = const Color(0xff7C3E66);
+Color primaryColor = const Color(0xff161922);
+Color secondaryColor = const Color(0xff3b479b);
+Color accentColor = Colors.grey;
 
 double kSpacing = 20.00;
 
